3d0d99af0e0ad75d98dcba451a5cd03a9c49ccfb
roza <mirian_mrh@hotmail.com>
2011-09-29 23:08:31 -0500

    Se edito el archivo README, se agrego extension markdown

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '382f9c374af0646848fcf460a40efd17800ff1d5deb5ea2440ca47829e56d3bbbfac970da9e7418a6d21d70ef6668ecac43e0754367f488f568e71a3a0f14153'
